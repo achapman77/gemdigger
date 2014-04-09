5262730c6464a4b58722925639c3b82a5a5605c7
Adam Chapman <achapman77@gmail.com>
2014-04-08 18:18:37 -0600

    Fixed comments controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,27 @@
 class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
+    has_many :votes, dependent: :destroy
     mount_uploader :image, ImageUploader
+
+    def up_votes
+    self.votes.where(value: 1).count
+    end
+
+    def down_votes
+    self.votes.where(value: -1).count
+    end
+
+    def points
+    self.votes.sum(:value).to_i
+    end
+
     belongs_to :user
     belongs_to :topic
 
     default_scope { order('created_at DESC') }
 
     validates :title, length: { minimum: 5 }, presence: true
-    validates :body, length: { minimum: 3 }, presence: true
+    validates :body, length: { minimum: 5 }, presence: true
     validates :topic, presence: true
     validates :user, presence: true
 end
